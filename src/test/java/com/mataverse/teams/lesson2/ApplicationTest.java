a29598e363a83e677f088579a3070f7cb22d24e8
onefanki94 <renaitrout@kakao.com>
2025-03-13 17:43:09 +0900

    second mission(one)

--- src/test/java/com/mataverse/teams/lesson2/ApplicationTest.java
+++ src/test/java/com/mataverse/teams/lesson2/ApplicationTest.java
@@ -1,7 +1,19 @@
 package com.mataverse.teams.lesson2;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest {
+
+    @Test
+    void DayofWeekPrinter(){
+        Application application = new Application();
+        int[] input = {1,2,4,5,9};
+        String value = application.dayWeek(input);
+        String result = "Monday,Tuesday,Thursday,Friday,Unknown";
+        Assertions.assertEquals(result, value);
+    }
 
 }
