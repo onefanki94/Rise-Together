946c5f03bcbb4e48fa317ae512d8ff8e6a26e58a
onefanki94 <renaitrout@kakao.com>
2025-03-14 15:49:27 +0900

    상속 및 캡슐화 연습 문제 추가

---
+++ src/main/java/com/mataverse/teams/chap02/Application.java
@@ -0,0 +1,8 @@
+package com.mataverse.teams.chap02;
+
+public class Application {
+    public static void main(String[] args) {
+        GradeStudent gradeStudent = new GradeStudent("John", 20,85);
+        gradeStudent.printStudentInfo();
+    }
+}
